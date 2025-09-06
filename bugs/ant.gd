f308c4807397d5146cfbaddea70e5801d8d0f8c4
Jacob-Hawksley <jacobmhawksley@gmail.com>
2025-09-06 17:55:24 +0100

    Made the rockets move and added aiming

--- bugs/ant.gd
+++ bugs/ant.gd
@@ -1,11 +1,15 @@
 extends CharacterBody2D
 @export var rocket: PackedScene = preload("res://rocket.tscn")
+@onready var marker = $Marker2D
 
 func _process(delta: float) -> void:
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
+	marker.rotate(marker.get_angle_to(get_local_mouse_position()))
+	marker.rotation += PI / 2
 
 func shoot():
 	var r = rocket.instantiate()
 	owner.add_child(r)
-	r.global_transform = $Marker2D.global_transform
+	r.transform = marker.global_transform
+	
--- bugs/rocket.gd
+++ bugs/rocket.gd
@@ -1,3 +1,12 @@
 extends RigidBody2D
-func _ready() -> void:
-	angular_velocity = 300
+@onready var explosioncollision = $Explosion/CollisionShape2D
+func _physics_process(delta: float) -> void:
+	move_local_y(-10)
+
+
+func _on_body_entered(body: Node) -> void:
+	print('a')
+	explosioncollision.disabled = false
+	visible = false
+	await get_tree().create_timer(1)
+	queue_free()
