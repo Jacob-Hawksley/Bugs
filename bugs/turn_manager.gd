b473c53db4d52d74e07080854c5fcfc0ab61f592
Jacob-Hawksley <jacobmhawksley@gmail.com>
2025-09-15 19:50:27 +0100

    Instantiated bugs

--- bugs/turn_manager.gd
+++ bugs/turn_manager.gd
@@ -1,9 +1,13 @@
 extends Node
 
-
+func _ready() -> void:
+	spawnteam1()
 
 func spawnteam1():
-	pass
+	if Main.map == 'crags':
+		var db = preload("res://DungBeetle.tscn").instantiate()
+		db.global_position = CragsData.DungBeetlePos1
+		
 
 func spawnteam2():
 	pass
