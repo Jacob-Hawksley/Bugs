5ab0ff0c0f56f97bc6c59a6caaeed7e237e6c3f9
Jacob-Hawksley <jacobmhawksley@gmail.com>
2025-09-16 19:50:57 +0100

    started to add scalability

--- bugs/main.gd
+++ bugs/main.gd
@@ -1,3 +1,3 @@
 extends Node
 
-var map = null
+var map = 'crags'
--- bugs/turn_manager.gd
+++ bugs/turn_manager.gd
@@ -1,13 +1,17 @@
 extends Node
-
+@export var dungbeetle: PackedScene = preload("res://DungBeetle.tscn")
 func _ready() -> void:
 	spawnteam1()
 
 func spawnteam1():
 	if Main.map == 'crags':
-		var db = preload("res://DungBeetle.tscn").instantiate()
+		var db = dungbeetle.instantiate()
+		self.add_child(db)
 		db.global_position = CragsData.DungBeetlePos1
+		db.visible = true
 		
+
+
 
 func spawnteam2():
 	pass
